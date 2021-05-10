a5dfc7140f4dc426fe35136e7652048790e7d93a
repl.it user <replituser@example.com>
2021-05-10 22:31:08 +0000

    Week 1 demos complete

--- main.py
+++
@@ -1,8 +0,0 @@
-print ("What is your name?")
-name = input ()
-print ("What is your age?")
-age = int(input())
-print("What is your bank balance?")
-balance = float(input())
-
-print("Welcome {}. You are said to be {} years old. Your bank balance is {}.".format(name, age, balance))
