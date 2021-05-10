6c00c64a4915380e6fc8d2d60e174f9e70544ac5
repl.it user <replituser@example.com>
2021-05-10 22:02:23 +0000

    Week 1 demos complete

---
+++ Demos/datatypes.py
@@ -0,0 +1,8 @@
+print ("What is your name?")
+name = input ()
+print ("What is your age?")
+age = int(input())
+print("What is your bank balance?")
+balance = float(input())
+
+print("Welcome {}. You are said to be {} years old. Your bank balance is {}.".format(name, age, balance))
--- main.py
+++
@@ -1,4 +0,0 @@
-print("EA Sports servers are trash")
-print("First step: git add .")
-print("Second step: git commit -m \"Your message here\"" )
-print("git push")
